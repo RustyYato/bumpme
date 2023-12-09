80e976efae8210a8fbfc5ffdfc7b292238600321
RustyYato <rustyyato@gmail.com>
2023-12-08 19:46:14 -0700

    improve perf harness

--- bump-test/src/main.rs
+++ bump-test/src/main.rs
@@ -1,23 +1,43 @@
 use std::hint::black_box;
 
 fn main() {
-    let start = std::time::Instant::now();
+    run(bumpme::Bump::new);
+    run(bumpalo::Bump::new);
+}
 
-    let mut bump = bumpme::Bump::new();
-    for i in 0..10_000_000_000u64 {
-        if i % 100_000 == 0 {
-            bump.reset();
-        }
-        bump.alloc_str(black_box("hello world"));
+pub trait BumpAlloc {
+    fn reset(&mut self);
+
+    #[allow(clippy::mut_from_ref)]
+    fn alloc_str(&self, s: &str) -> &mut str;
+}
+
+impl BumpAlloc for bumpme::Bump {
+    fn reset(&mut self) {
+        self.reset()
     }
-    drop(bump);
-    dbg!(start.elapsed());
 
+    fn alloc_str(&self, s: &str) -> &mut str {
+        self.alloc_str(s)
+    }
+}
+
+impl BumpAlloc for bumpalo::Bump {
+    fn reset(&mut self) {
+        self.reset()
+    }
+
+    fn alloc_str(&self, s: &str) -> &mut str {
+        self.alloc_str(s)
+    }
+}
+
+fn run<F: Fn() -> A, A: BumpAlloc>(mk_bump: F) {
     let start = std::time::Instant::now();
 
-    let mut bump = bumpalo::Bump::new();
+    let mut bump = mk_bump();
     for i in 0..10_000_000_000u64 {
-        if i % 100_000 == 0 {
+        if i % 10_000 == 0 {
             bump.reset();
         }
         bump.alloc_str(black_box("hello world"));
