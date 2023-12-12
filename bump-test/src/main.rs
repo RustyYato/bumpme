e7a421bb5a3b8e157ea7907874f037a6dbea37c4
RustyYato <rustyyato@gmail.com>
2023-12-12 07:52:53 -0700

    add throughput and latency measurements

--- bump-test/src/main.rs
+++ bump-test/src/main.rs
@@ -1,8 +1,10 @@
 use std::hint::black_box;
 
 fn main() {
-    run(bumpme::Bump::new);
-    run(bumpalo::Bump::new);
+    throughput(bumpme::Bump::new);
+    throughput(bumpalo::Bump::new);
+    latency(bumpme::Bump::new);
+    latency(bumpalo::Bump::new);
 }
 
 pub trait BumpAlloc {
@@ -32,7 +34,7 @@ impl BumpAlloc for bumpalo::Bump {
     }
 }
 
-fn run<F: Fn() -> A, A: BumpAlloc>(mk_bump: F) {
+fn throughput<F: Fn() -> A, A: BumpAlloc>(mk_bump: F) {
     let start = std::time::Instant::now();
 
     let mut bump = mk_bump();
@@ -44,4 +46,27 @@ fn run<F: Fn() -> A, A: BumpAlloc>(mk_bump: F) {
     }
     drop(bump);
     dbg!(start.elapsed());
+}
+
+fn latency<F: Fn() -> A, A: BumpAlloc>(mk_bump: F) {
+    let mut timings = [std::time::Duration::ZERO; 4];
+
+    let mut bump = mk_bump();
+    for i in 0..1_000_000_000u64 {
+        if i % 10_000 == 0 {
+            bump.reset();
+        }
+
+        let start = std::time::Instant::now();
+        bump.alloc_str(black_box("hello world"));
+        let time = start.elapsed();
+        for prev in timings.iter_mut() {
+            if time > *prev {
+                *prev = time;
+                break;
+            }
+        }
+    }
+    drop(bump);
+    dbg!(timings);
 }
